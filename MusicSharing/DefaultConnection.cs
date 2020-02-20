139b654943527f3d485d09ed6a417a41f8a62a32
Rasitha Gamage <g.rasitha@gmail.com>
2020-02-20 19:55:47 +0530

    Add project files.

---
+++ MusicSharing/DefaultConnection.cs
@@ -0,0 +1,20 @@
+namespace MusicSharing
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DefaultConnection : DbContext
+    {
+        public DefaultConnection()
+            : base("name=DefaultConnection")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ MusicSharing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicSharing.Startup))]
+namespace MusicSharing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
