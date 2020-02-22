42bab52c4341f72b3b203ef3f4fb7065288dc979
kalpa wijesooriya <kalpa.wijesooriya@yahoo.com>
2020-02-22 16:42:02 +0530

    add Misic Class

--- MusicSharing/Controllers/AccountController.cs
+++ MusicSharing/Controllers/AccountController.cs
@@ -449,7 +449,7 @@ namespace MusicSharing.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("MainPage", "Home");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
